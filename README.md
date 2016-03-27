4e28c01dbf98010d1760d742ea0092c74918b9ef
naoki kodama <naoki.koda@gmail.com>
2016-03-28 00:51:25 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hubot-pushbullet-script
+
+hubot で受けた全ての発言を指定した pushbullet クライアントへ通知するスクリプト
+
+* hubot の scripts 配下に pushbullet.js を配置
+* $ npm install pushbullet
+* 以下の環境変数を追加して hubot を起動
+```
+HUBOT_PUSHBULLET_API_KEY="xxx"
+HUBOT_PUSHBULLET_EMAIL="xxx@gmail.com"
+```
---
+++ pushbullet.js
@@ -0,0 +1,14 @@
+var PushBullet = require('pushbullet');
+
+var Token = process.env.HUBOT_PUSHBULLET_API_KEY;
+var Email = process.env.HUBOT_PUSHBULLET_EMAIL;
+var pusher = new PushBullet(Token);
+
+module.exports = function(robot){
+  robot.hear('', function(msg){
+    var user = msg.message['user']['name'];
+    var message = msg.message['text'];
+    pusher.note(Email, "DevHub", user + ": " + message, function(error, response) {
+    });
+  });
+};
